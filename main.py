c7dff49489f5ffdc24fe104bf7dbb726d297eead
dxria <dariakuznetsova@knu.ua>
2023-02-15 11:52:20 +0200

    initialising and setting the screen

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pygame
+
+pygame.init()
+
+screen_width = 500
+screen_height = 500
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('arkanoid')
+
+run = True
+while run:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
