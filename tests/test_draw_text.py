d2c7219f30afe42cd7ed66aa5b7f9c664175db4b
Mariia Romanova <romanovamr0409@icloud.com>
2023-04-10 23:01:15 +0300

    invalid input testing (for draw_text)

--- tests/test_draw_text.py
+++ tests/test_draw_text.py
@@ -11,12 +11,25 @@ def screen():
 @pytest.mark.parametrize("text, font, color, x, y, expected_color", [
     ("Hello World", pygame.font.Font(None, 60), (255, 255, 255), 100, 100, (255, 255, 255, 255)),
     ("", pygame.font.Font(None, 60), (255, 255, 255), 200, 200, (0, 0, 0, 255)),
-    ("This text is too large to fit on the screen", pygame.font.Font(None, 60), (255, 255, 255), 400, 400, (255, 255, 255, 255)),
+    ("This text is too large to fit on the screen.................", pygame.font.Font(None, 60), (255, 255, 255), 400, 400, (255, 255, 255, 255)),
     ("!@#%^&*()-_=+[];:',.<>/?|`~", pygame.font.Font(None, 60), (255, 255, 255), 100, 100, (255, 255, 255, 255)),
     ("0123456789", pygame.font.Font(None, 60), (255, 255, 255), 100, 100, (255, 255, 255, 255)),
 ])
 def test_draw_text(screen, text, font, color, x, y, expected_color):
     draw_text(text, font, color, screen, x, y)
     assert screen.get_at((x+5, y+5)) == expected_color
+
+
+@pytest.mark.parametrize("text, font, color, screen, x, y", [
+    ("invalid color", 60, "invalid color", screen, 100, 100),
+        (1, pygame.font.Font(None, 60), (255, 255, 255), screen, 100, 100),
+        ("invalid screen", pygame.font.Font(None, 60), (255, 255, 255), "invalid screen", 100, 100),
+        ("invalid x", pygame.font.Font(None, 60), (255, 255, 255), screen, "invalid x", 100),
+        ("invalid font", "invalid font", (255, 255, 255), screen, 100, 100),
+        ("invalid y", pygame.font.Font(None, 60), (255, 255, 255), screen, 100, "invalid y"),
+])
+def test_draw_text_invalid_input(text, font, color, screen, x, y):
+    with pytest.raises(ValueError):
+            draw_text(text, font, color, screen, x, y)
 
     
