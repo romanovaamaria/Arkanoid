5a47a5847c9574507ed623e6e4af14ae43e4dca3
Марія Романова <99257767+romanovaamaria@users.noreply.github.com>
2023-04-11 03:06:22 +0300

    Delete test_game_class.py

--- tests/test_game_class.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import pygame
-from main import game
-
-def test_game_invalid_input():
-    #Test if game raises TypeError when an invalid input is given
-    with pytest.raises(TypeError):
-        game('level 1')
